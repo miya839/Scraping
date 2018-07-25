81aac2b549bbd69ae5acb018f67f65c1dabf9d70
宮本栄作 <>
2018-07-25 23:18:30 +0900

    update

--- getWeather.py
+++ getWeather.py
@@ -5,16 +5,14 @@ def get_stockPrice():
     base_url = "https://tenki.jp/forecast/5/24/5210/21201/"
     ret = requests.get(base_url)
     soup =  BeautifulSoup(ret.content, "lxml")
-    today = soup.find("section", {'class':'today-weather'}).text
-    tommorow = soup.find("section", {'class':'tomorrow-weather'}).text
-    print (tommorow)
+    
     week = soup.find("section",{'class':'forecast-point-week-wrap'})
-    print (len(week))
     for i in range(len(week)):
-        print (week.find_all("td",{'class':'cityday'})[i].text)
-        print ("最高",week.find_all("p",{'class':'high-temp'})[i].text,)
-        print ("最低",week.find_all("p",{'class':'low-temp'})[i].text,)
-        print ("降水確率",week.find_all("p",{'class':'precip'})[i].text,)
+        tmp = week.find_all("td",{'class':'cityday'})[i].text.split()
+        print (tmp[0],tmp[1])
+        print ("最高",week.find_all("p",{'class':'high-temp'})[i].text)
+        print ("最低",week.find_all("p",{'class':'low-temp'})[i].text)
+        print ("降水確率",week.find_all("p",{'class':'precip'})[i].text)
         
 if __name__ == "__main__":
     get_stockPrice()
